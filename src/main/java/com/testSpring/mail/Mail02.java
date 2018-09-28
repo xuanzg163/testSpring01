0e869f10119555679462a24671e2f1aaccd7d617
xuanzg163 <xuan_zg@163.com>
2018-09-28 19:49:25 +0800

    1、通过Javamail实现HTML邮件发送

--- src/main/java/com/testSpring/mail/Mail02.java
+++ src/main/java/com/testSpring/mail/Mail02.java
@@ -2,13 +2,15 @@ package com.testSpring.mail;
 
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import java.util.Properties;
 
 /**
- *  实现 简单发送邮件
+ *  实现发送HTML邮件内容
  */
-public class Mail {
+public class Mail02 {
     public static void main(String[] args) throws MessagingException {
 
         //卫星
@@ -38,12 +40,20 @@ public class Mail {
         message.setFrom(from);
 
         //接收人
-        Address receiver = new InternetAddress("csc17749731709@163.com ");
+        Address receiver = new InternetAddress("csc17749731709@163.com");
         message.setRecipient(Message.RecipientType.TO,receiver);
 
-        //内容
-        message.setSubject("!!!");
-        message.setText("hi this is javamail test!!!");
+        //html内容
+        message.setSubject("这是带有HTML内容的文件");
+
+        Multipart multipart = new MimeMultipart();
+        BodyPart html = new MimeBodyPart();
+        html.setContent("<img src=\"http://wx4.sinaimg.cn/mw690/7d63d28agy1fvph5g8kczg208c08c0wi.gif\" />"
+                ,"text/html;charset=utf-8");
+
+        multipart.addBodyPart(html);
+        message.setContent(multipart);
+
 
         //送卫星上天
         Transport.send(message);
